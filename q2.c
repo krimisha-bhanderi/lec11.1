2a509dbb9943f6f786a7edeaa4157f8e630075ad
krimisha <krimisha025@gmail.com>
2023-12-25 10:39:19 +0000

    programm

---
+++ q2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	int *p;
+	int *q;
+
+	printf("enter the value of a:");
+	scanf("%d",&a);
+
+	printf("enter the value of b:");
+	scanf("%d",&b);q
+	
+	p=&a;
+	q=&b;
+	c=*p;
+	*p=*q;
+	*q=c;
+
+	printf("After swapping:-\n");
+	printf("a : %d\n",*p);
+	printf("b : %d\n",*q);
+
+}
+
